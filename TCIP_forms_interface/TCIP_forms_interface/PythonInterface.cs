ee7e7965497825822f9d78d5ff4a71228db5dcb0
Oporto <pdevasconcelloso@wpi.edu>
2019-04-23 21:31:46 -0400

    added run_python_code method

--- TCIP_forms_interface/TCIP_forms_interface/PythonInterface.cs
+++ TCIP_forms_interface/TCIP_forms_interface/PythonInterface.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +22,30 @@ namespace TCIP_forms_interface
 
         private int[,] seeGrid()
         {
-            return new int[6, 7];
+            String stream = run_python_code("python", "wagner.py");
+            var grid = new int[6, 7];
+            //TODO: parse strem into grid
+            return grid;
+        }
+
+        private String run_python_code(string cmd, string args)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "PATH_TO_PYTHON_EXE";
+            start.Arguments = string.Format("\"{0}\" \"{1}\"", cmd, args);
+            start.UseShellExecute = false;
+            start.CreateNoWindow = true; 
+            start.RedirectStandardOutput = true;
+            start.RedirectStandardError = true;
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string stderr = process.StandardError.ReadToEnd();
+                    string result = reader.ReadToEnd();
+                    return result;
+                }
+            }
         }
     }
 }
